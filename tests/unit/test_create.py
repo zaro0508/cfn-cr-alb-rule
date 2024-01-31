6fef55341ec8edb800cfaf02540b5dbd0d56bc9d
bhoff <bruce.hoff@sagebase.org>
2024-01-31 09:52:14 -0800

    add session cookie name prefix as stack parameter and append instance id for uniqueness

--- tests/unit/test_create.py
+++ tests/unit/test_create.py
@@ -12,7 +12,7 @@ from ruler import app
 
 class TestCreate(unittest.TestCase):
 
-  mock_create_response = {"Rules":[{"RuleArn":"arn:aws:elasticloadbalancing:us-east-1:012345678901:listener-rule/app/sc135-poc/bf54cc972d64237b/8ad4c71091181c60/62caa6dfa73dbce8","Priority":"7","Conditions":[{"Field":"path-pattern","Values":["/i-049f8b7f35ef87673"],"PathPatternConfig":{"Values":["/i-049f8b7f35ef87673"]}}],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:us-east-1:012345678901:targetgroup/TargetGroup-i-049f8b7f35ef87673/32ee340e330ca6e2","Order":2,"ForwardConfig":{"TargetGroups":[{"TargetGroupArn":"arn:aws:elasticloadbalancing:us-east-1:012345678901:targetgroup/TargetGroup-i-049f8b7f35ef87673/32ee340e330ca6e2","Weight":1}],"TargetGroupStickinessConfig":{"Enabled":False}}},{"Type":"authenticate-oidc","AuthenticateOidcConfig":{"Issuer":"https://repo-prod.prod.sagebase.org/auth/v1","AuthorizationEndpoint":"https://repo-prod.prod.sagebase.org/auth/v1","TokenEndpoint":"https://qtg2zn2bbf.execute-api.us-east-1.amazonaws.com/token","UserInfoEndpoint":"https://repo-prod.prod.sagebase.org/auth/v1/oauth2/userinfo","ClientId":"100050","SessionCookieName":"AWSELBAuthSessionCookie","Scope":"openid","SessionCookieName":"my-cookie","SessionTimeout":604800,"AuthenticationRequestExtraParams":{"claims":"{\"id_token\":{\"userid\":{\"essential\":true}},\"userinfo\":{\"userid\":{\"essential\":true}}}"},"OnUnauthenticatedRequest":"authenticate"},"Order":1}],"IsDefault":False}]}
+  mock_create_response = {"Rules":[{"RuleArn":"arn:aws:elasticloadbalancing:us-east-1:012345678901:listener-rule/app/sc135-poc/bf54cc972d64237b/8ad4c71091181c60/62caa6dfa73dbce8","Priority":"7","Conditions":[{"Field":"path-pattern","Values":["/i-049f8b7f35ef87673"],"PathPatternConfig":{"Values":["/i-049f8b7f35ef87673"]}}],"Actions":[{"Type":"forward","TargetGroupArn":"arn:aws:elasticloadbalancing:us-east-1:012345678901:targetgroup/TargetGroup-i-049f8b7f35ef87673/32ee340e330ca6e2","Order":2,"ForwardConfig":{"TargetGroups":[{"TargetGroupArn":"arn:aws:elasticloadbalancing:us-east-1:012345678901:targetgroup/TargetGroup-i-049f8b7f35ef87673/32ee340e330ca6e2","Weight":1}],"TargetGroupStickinessConfig":{"Enabled":False}}},{"Type":"authenticate-oidc","AuthenticateOidcConfig":{"Issuer":"https://repo-prod.prod.sagebase.org/auth/v1","AuthorizationEndpoint":"https://repo-prod.prod.sagebase.org/auth/v1","TokenEndpoint":"https://qtg2zn2bbf.execute-api.us-east-1.amazonaws.com/token","UserInfoEndpoint":"https://repo-prod.prod.sagebase.org/auth/v1/oauth2/userinfo","ClientId":"100050","SessionCookieName":"AWSELBAuthSessionCookie-i-049f8b7f35ef87673","Scope":"openid","SessionCookieName":"my-cookie","SessionTimeout":604800,"AuthenticationRequestExtraParams":{"claims":"{\"id_token\":{\"userid\":{\"essential\":true}},\"userinfo\":{\"userid\":{\"essential\":true}}}"},"OnUnauthenticatedRequest":"authenticate"},"Order":1}],"IsDefault":False}]}
   mock_properties = [
     'i-049f8b7f35ef87673',
     'arn:aws:elasticloadbalancing:us-east-1:012345678901:targetgroup/TargetGroup-i-049f8b7f35ef87673/32ee340e330ca6e2',
@@ -25,7 +25,7 @@ class TestCreate(unittest.TestCase):
     'token-endpoint',
     'user-info-endpoint',
     'client-id',
-    'my-cookie',
+    'my-cookie-prefix',
     '3600'
   ]
 
